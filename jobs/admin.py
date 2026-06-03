54ca7949e119432be578624be850ddaed3a2ff44
sahala <sahalashanmekkilery@gmail.com>
2026-06-03 17:22:50 +0530

    Day 34 AI conversation storage transcript system and call logs

--- jobs/admin.py
+++ jobs/admin.py
@@ -3,5 +3,15 @@ from .models import Job,Application
 
 admin.site.register(Job)
 admin.site.register(Application)
+from .models import (
+    AIInterviewSession,
+    AIQuestion,
+    AIAnswer,
+    CallLog
+)
 
+admin.site.register(AIInterviewSession)
+admin.site.register(AIQuestion)
+admin.site.register(AIAnswer)
+admin.site.register(CallLog)
 # Register your models here.
--- jobs/models.py
+++ jobs/models.py
@@ -200,3 +200,78 @@ class AICall(models.Model):
 
     def __str__(self):
         return f"{self.candidate.email} - {self.status}"
+class AIInterviewSession(models.Model):
+
+    candidate = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    job = models.ForeignKey(
+        Job,
+        on_delete=models.CASCADE
+    )
+
+    status = models.CharField(
+        max_length=50,
+        default='started'
+    )
+
+    started_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    ended_at = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+class AIQuestion(models.Model):
+
+    session = models.ForeignKey(
+        AIInterviewSession,
+        on_delete=models.CASCADE
+    )
+
+    question_text = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+class AIAnswer(models.Model):
+
+    question = models.ForeignKey(
+        AIQuestion,
+        on_delete=models.CASCADE
+    )
+
+    answer_text = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+class CallLog(models.Model):
+
+    candidate = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    job = models.ForeignKey(
+        Job,
+        on_delete=models.CASCADE
+    )
+
+    triggered_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='triggered_calls'
+    )
+
+    reason = models.CharField(
+        max_length=255
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
