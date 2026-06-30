641eb39e60afb9b7be54ff151b843f8b7fee26da
sahala <sahalashanmekkilery@gmail.com>
2026-06-30 18:48:05 +0530

    Configure environment variables with dotenv

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
